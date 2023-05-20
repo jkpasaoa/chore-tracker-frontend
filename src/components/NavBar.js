53ff1f6846e4655c9daf6aca865fb1afb733f109
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-19 23:21:41 -0400

    adjusted navbar and its CSS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../index.css"
+
 function NavBar() {
   return (
-    <div>
-      <h1>NavBar is all over the pages</h1>
-    </div>
+    <nav className="navbar">
+      <div className="nav-links">
+        <ul>
+          <li className="logo">
+            <Link to="/">Chore Wizard</Link>
+          </li>
+          <li>
+            <Link to="/chores">View All Chores</Link>
+          </li>
+          <li className="add-new-chore">
+            <Link to="/chores/new">Add New Chore</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
