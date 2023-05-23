7745f5cfea954b1383a642f87c0c8a73f9409f28
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-23 02:59:21 -0400

    added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom";
+import { Image } from "react-bootstrap";
+import washingHands from "../assets/washingHands.jpg";
 
 function Home() {
   return (
     <div className="Home" style={{ maxWidth: '500px', margin: '0 auto', paddingTop: '50px' }}>
       <h2>Welcome</h2>
       <h3>To Chore Wizard!</h3>
-      Click to<span>&nbsp;</span>
-      <Link to="/chores">
-        View All Chores
-      </Link>
+      <p className="text-left">
+        <br />
+        Click to<span>&nbsp;</span>
+        <Link to="/chores">
+          View All Chores
+        </Link>
+      </p>
+      <br />
+      <Image
+        src={washingHands}
+        alt="washhands"
+        Square
+        width="320"
+      />
     </div>
   );
 }
