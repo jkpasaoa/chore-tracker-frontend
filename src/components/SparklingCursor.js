b00b17e4afdfe5b831c6ab76698cea1cb8ee220b
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-24 00:17:01 -0400

    sparkling component

---
+++ src/components/SparklingCursor.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import MouseParticles from 'react-mouse-particles'
+
+class SparklingCursor extends Component {
+  render() {
+    return (
+      <>
+        <div>...</div>
+        <MouseParticles g={1} num={6} color="random" cull="stats,image-wrapper" level={6} />
+      </>
+    )
+  }
+}
+export default SparklingCursor;
+
+// Code for SparklingCursor component obtained
+// from: https://github.com/lindelof/react-mouse-particles
